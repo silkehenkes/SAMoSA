Fixed some minor Hessian bugs. Added functinality to add stiffness by hand. Cleaned up calculation and plotting of projections, added participation ratio.

Dynamics.py
@@ -47,7 +47,18 @@ class Dynamics(Configuration):
 		# If that's out of the way, nothing is being relabeled. These are our labels once and for all
 		useparts = self.getUseparts(usetype,0)
 		# This is the crucial one for the Hessian
-		rval0 = np.mean(self.rval[:,useparts,:],axis=0)
+		# do note PBC issues ... so more complicated
+		if self.geom.periodic:
+			rval0 = np.zeros((len(useparts),3))
+			ct=0
+			for id in useparts:
+				dr = self.geom.ApplyPeriodic12(self.rval[:,id,:],self.rval[0,id,:])
+				rval0[ct] = np.average(dr,axis=0) + self.rval[0,id,:]
+				ct+=1
+			# put back into box if necessary
+			self.geom.ApplyPeriodic2d(rval0)
+		else:
+			rval0 = np.mean(self.rval[:,useparts,:],axis=0)
 		# Less meaningful, but indulge me
 		vval0 = np.mean(self.vval[:,useparts,:],axis=0)
 		nval0 = np.mean(self.nval[:,useparts,:],axis=0)
@@ -56,9 +67,9 @@ class Dynamics(Configuration):
 		ptype0 = self.ptype[0,useparts]
 		flag0 = self.flag[0,useparts]
 		
-		#self.fromPython(kwargs["param"],kwargs["rval"],kwargs["vval"],kwargs["nval"],kwargs["radii"],kwargs["ptype"],kwargs["flag"])
-		#def fromPython(self,param,rval,vval,nval,radius,ptype,flag):
-		averageChild = Configuration(initype="fromPython",param=self.param,rval=rval0,vval=vval0,nval=nval0,radii=radius0,ptype=ptype0,flag=flag0)
+		#self.fromPython(kwargs["param"],kwargs["rval"],kwargs["vval"],kwargs["nval"],kwargs["radii"],kwargs["ptype"],kwargs["flag"],kwargs["makeCellList"],kwargs["redobox"])
+		#def fromPython(self,param,rval,vval,nval,radius,ptype,flag,makeCellList=True,redobox=False):
+		averageChild = Configuration(initype="fromPython",param=self.param,rval=rval0,vval=vval0,nval=nval0,radii=radius0,ptype=ptype0,flag=flag0,makeCellList=True,redobox=False)
 		return averageChild
 		
 	# Tracking a subset of particles: This identifies the relevant ones
@@ -722,9 +733,6 @@ class Dynamics(Configuration):
 			#proj2=np.zeros((3*Hessian.N,self.Nsnap))
 			#print Hessian.eigvec[0:2*Hessian.N:2,0]
 			#print Hessian.eigvec[0:3*Hessian.N:3,1]
-			# Check out what's going on here
-			transcomp=np.zeros((self.Nsnap,3))
-			velcomp=np.zeros((self.Nsnap,3))
 			for u in range(self.Nsnap):
 				#dr=self.geom.ApplyPeriodic2d(rnew-hessnew)
 				if self.geom.periodic:
@@ -745,7 +753,7 @@ class Dynamics(Configuration):
 			self.proj2av=np.sum(self.proj**2,axis=1)
 			self.projv2av=np.sum(self.projv**2,axis=1)
 			
-		return self.proj,self.projv,self.proj2av,self.projv2av,transcomp,velcomp
+		return self.proj,self.projv,self.proj2av,self.projv2av
 			
 	def plotProjections(self,Hessian,nmodes=5):
 	  
@@ -755,7 +763,7 @@ class Dynamics(Configuration):
 		plt.figure()
 		for u in range(nmodes):
 			c=next(color)
-			plt.plot(tval,self.proj[u,:],'.-',color=c,label='mode '+str(u))
+			plt.plot(tval,self.proj[u,:],'-',color=c,label='mode '+str(u))
 		if nmodes<10:
 			plt.legend()
 		plt.xlabel('time')
@@ -766,7 +774,7 @@ class Dynamics(Configuration):
 		plt.figure()
 		for u in range(nmodes):
 			c=next(color)
-			plt.plot(tval,self.projv[u,:],'.-',color=c,label='mode '+str(u))
+			plt.plot(tval,self.projv[u,:],'-',color=c,label='mode '+str(u))
 		if nmodes<10:
 			plt.legend()
 		plt.xlabel('time')
@@ -776,15 +784,18 @@ class Dynamics(Configuration):
 		
 		omega = np.sqrt(np.abs(Hessian.eigval))
 		plt.figure()
-		plt.plot(omega,0.5*Hessian.eigval*self.proj2av,'.-r',label='projection')
+		nrm=np.average(0.5*Hessian.eigval*self.proj2av)
+		energy=0.5*Hessian.eigval*self.proj2av/nrm
+		plt.semilogy(omega,energy,'.r',label='projection')
 		plt.xlabel(r'$\omega$')
 		plt.ylabel('Energy')
-		plt.xlim(0,12)
+		#plt.xlim(0,12)
 		#plt.ylim(1e-10,1e-5)
 		plt.title('Energy from displacement projections')
+
 		
 		plt.figure()
-		plt.plot(omega,self.proj2av/np.sum(self.proj2av),'.-r',label='displacements')
+		plt.semilogy(omega,self.proj2av/np.sum(self.proj2av),'.r',label='displacements')
 		plt.xlabel(r'$\omega$')
 		plt.ylabel('projection square')
 		#plt.xlim(-0.01,12)
@@ -792,12 +803,14 @@ class Dynamics(Configuration):
 		plt.title('Square projections (normalised)')
 		
 		plt.figure()
-		plt.plot(omega,self.projv2av/np.sum(self.projv2av),'.-g',label='velocities')
+		plt.semilogy(omega,self.projv2av/np.sum(self.projv2av),'.g',label='velocities')
 		plt.xlabel(r'$\omega$')
 		plt.ylabel('projection square')
 		#plt.xlim(-0.01,12)
 		plt.legend()
 		plt.title('Square projections (normalised)')
+
+		return energy
 		
 		
 		

Hessian.py
@@ -20,6 +20,20 @@ class Hessian:
 		#self.inter=self.conf.inter
 		self.debug=debug
 		print("Checking for wrap: " + str(np.amin(self.conf.rval[:,0])))
+		
+		tps=list(np.unique(self.conf.ptype))
+		print(tps)
+		if self.debug:
+			plt.figure()
+			cols=['k','r','b','g','m'] # allowed to crash if we ever have more thatn 5 types ...
+		self.numtypes=np.zeros((len(tps),))
+		for tp in tps:
+			idx = np.where(self.conf.ptype==tp)[0]
+			print(len(idx))
+			self.numtypes[tp-1] = len(idx)
+			if self.debug:
+				plt.plot(self.conf.rval[idx,0],self.conf.rval[idx,1],marker='o',linestyle='none',color=cols[tp-1])
+		print(self.numtypes)
 	
 	def setRattlers(rattlers):
 		self.rattlers = rattlers
@@ -27,7 +41,7 @@ class Hessian:
         
 	
 	# This needs to be one of the single-frame configurations, create check for it
-	def makeMatrix(self,dim=2,fixBorder=True,btype = 2,typeweights = 'none',addRestoring=True,ksurf=10.0,addCurvature=False):
+	def makeMatrix(self,dim=2,fixBorder=True,btype = 2,typeweights = 'none',pairstiff='none',addRestoring=True,ksurf=10.0,addCurvature=False):
 		# Dimensions of the problem
 		self.dim = dim
 		# Check what dimension we are after
@@ -87,7 +101,10 @@ class Hessian:
 				fval=np.sum(fvec*nij,axis=1)
 				fav+=sum(fval)
 				# Stiffnesses
-				kij=self.conf.inter.getStiffness(i,neighbours,drvec,radi,radj)
+				if pairstiff=='auto':
+					kij=self.conf.inter.getStiffness(i,neighbours,drvec,radi,radj)
+				else:
+					kij=np.zeros((len(neighbours)))
 				# equilibrium distances are given by dr already
 				
 				# Now we are filling the matrix in either 3d or 2d
@@ -108,6 +125,11 @@ class Hessian:
 						else:
 							weii = 1.0
 							weij = 1.0
+						if not pairstiff == 'auto':
+							tpi = int(self.conf.ptype[i])-1
+							tpj = int(self.conf.ptype[neighbours[j]])-1
+							kij[j] = pairstiff[tpi][tpj]
+							fval[j] = fval[j]*kij[j]
 						n=nij[j,:]
 						N=Normal[neighbours[j],:]
 						subsquare=np.zeros((3,3))
@@ -189,6 +211,11 @@ class Hessian:
 						else:
 							weii = 1.0
 							weij = 1.0
+						if not pairstiff == 'auto':
+							tpi = int(self.conf.ptype[i])-1
+							tpj = int(self.conf.ptype[neighbours[j]])-1
+							kij[j] = pairstiff[tpi][tpj]
+							fval[j] = fval[j]*kij[j]
 						n=nij[j,:]
 						subsquare=np.zeros((2,2))
 						# xx, xy and xz
@@ -228,9 +255,9 @@ class Hessian:
 		# Eigenvalues and eigenvectors
 		# Only symmetrise to calculate - for clarity and debugging above
 		HessianSym=0.5*(self.Hessian+np.transpose(self.Hessian))
-		if self.debug:
-			plt.figure()
-			plt.pcolor(HessianSym)
+		#if self.debug:
+		#	plt.figure()
+		#	plt.pcolor(HessianSym)
 		#HessianASym=0.5*(self.Hessian-np.transpose(self.Hessian))
 		#print HessianASym
 		# Use routines for hermitian eigenvector decomposition
@@ -239,6 +266,11 @@ class Hessian:
 		self.eigval, self.eigvec = LA.eigh(HessianSym)
 		print("The smallest eigenvalue is: " + str(np.amin(self.eigval)))
 		#print(self.eigval)
+		# Some obvious statistics here
+		# participation ratio
+		Qpart=np.zeros((len(self.eigval),))
+		for u in range(len(self.eigval)):
+			Qpart[u] = np.sum(np.abs(self.eigvec[:,u]))**2/(self.dim*self.N)
 		# Crosscheck on sanity
 		if self.dim == 3:
 			wx=np.zeros((self.N,))
@@ -257,14 +289,14 @@ class Hessian:
 				#print (wx)
 				#print (wy)
 				#print (wz)
-			return wx,wy,wz
+			return Qpart,wx,wy,wz
 		else:
 			wx=np.zeros((self.N,))
 			wy=np.zeros((self.N,))
 			for u in range(self.N):
-					# dimensional contributions
-					wx[u]=np.sum(self.eigvec[0:2*self.N:2,u]**2)
-					wy[u]=np.sum(self.eigvec[1:2*self.N:2,u]**2)
+				# dimensional contributions
+				wx[u]=np.sum(self.eigvec[0:2*self.N:2,u]**2)
+				wy[u]=np.sum(self.eigvec[1:2*self.N:2,u]**2)
 			if self.debug:
 				# start with some debugging output
 				plt.figure()
@@ -272,12 +304,12 @@ class Hessian:
 				plt.plot(eigrank,self.eigval,'.-')
 				#print (wx)
 				#print (wy)
-			return wx,wy
+			return Qpart,wx,wy
 		
 	# Unlike its 3d equivalent, this is more of a debugging situation
 	def plotModes2d(self,modelabels,omegamax=3.0,npts=100):
 		plt.figure()
-		n = np.ceil(len(modelabels)**0.5)
+		n = int(np.ceil(len(modelabels)**0.5))
 		ct = 1
 		for u in modelabels:
 			xix = self.eigvec[0:2*self.N:2,u]