Corrected several buts in VelAuto and SelfInt when the 'complicated' relabeling is in use. Tweaked the vel Fourier yet again for range and normalisation with experiment ...

Configuration.py
@@ -721,7 +721,7 @@ class Configuration:
 		# Note to self: only low q values will be interesting in any case. 
 		# The stepping is in multiples of the inverse box size. Assuming a square box.
 		print("Fourier transforming velocities")
-		dq=2*np.pi/L
+		dq=np.pi/L
 		nq=int(qmax/dq)
 		print("Stepping Fourier transform with step " + str(dq)+ ", resulting in " + str(nq)+ " steps.")
 		qx, qy, qrad, ptsx, ptsy=self.makeQrad(dq,qmax,nq)
@@ -738,6 +738,7 @@ class Configuration:
 		else:
 			useparts = self.getUseparts(usetype,whichframe)
 			N = len(useparts)
+			print(N)
 			for kx in range(nq):
 				for ky in range(nq):
 					fourierval[kx,ky,0]=np.sum(np.exp(1j*(qx[kx]*self.rval[whichframe,useparts,0]+qy[ky]*self.rval[whichframe,useparts,1]))*self.vval[whichframe,useparts,0])/N
@@ -746,7 +747,7 @@ class Configuration:
 		# Sq = \vec{v_q}.\vec{v_-q}, assuming real and symmetric
 		# = \vec{v_q}.\vec{v_q*} = v
 		Sq=np.real(fourierval[:,:,0])**2+np.imag(fourierval[:,:,0])**2+np.real(fourierval[:,:,1])**2+np.imag(fourierval[:,:,1])**2
-		Sq=N*Sq
+		#Sq=N*Sq
 		# Produce a radial averaging to see if anything interesting happens
 		nq2=int(2**0.5*nq)
 		Sqrad=np.zeros((nq2,))

Dynamics.py
@@ -246,15 +246,10 @@ class Dynamics(Configuration):
 			else:
 				self.v2av[u]=np.sum(np.sum((self.vval[u,self.usethese,:])**2,axis=1),axis=0)/(self.Ntrack)
 				vnormed[u,:,:]=self.vval[u,self.usethese,:]/np.sqrt(self.v2av[u])
-				
+		print(vnormed)
 		for u in range(self.Nsnap):
 			smax=self.Nsnap-u
-			if self.complicated:
-				print("Dynamics::getVelAuto - Warning: Complicated relabeling: This is slow, make sure to only track a few particles") 
-				for s in range(smax):
-					self.velauto[u] += np.sum(vnormed[s,:,0]*vnormed[u+s,:,0]+vnormed[s,:,1]*vnormed[u+s,:,1]+vnormed[s,:,2]*vnormed[u+s,:,2])/(self.Ntrack*smax)
-			else:
-				self.velauto[u]=np.sum(np.sum((vnormed[:smax,:,0]*vnormed[u:,:,0]+vnormed[:smax,:,1]*vnormed[u:,:,1]+vnormed[:smax,:,2]*vnormed[u:,:,2]),axis=1),axis=0)/(self.Ntrack*smax)
+			self.velauto[u]=np.sum(np.sum((vnormed[:smax,:,0]*vnormed[u:,:,0]+vnormed[:smax,:,1]*vnormed[u:,:,1]+vnormed[:smax,:,2]*vnormed[u:,:,2]),axis=1),axis=0)/(self.Ntrack*smax)
 
                                 
 		xval=np.linspace(0,self.Nsnap*self.param.dt*self.param.dump['freq'],num=self.Nsnap)
@@ -465,14 +460,15 @@ class Dynamics(Configuration):
 						ps = list(self.usethese[s,:])
 						pu = list(self.usethese[u+s,:])
 						if self.geom.periodic:
-							SelfInt[u] += np.sum(np.exp(1.0j*qval[0]*(self.geom.ApplyPeriodicX(-self.rval[s,ps,0]+self.rval[u+s,pu,0]))+1.0j*qval[1]*(self.geom.ApplyPeriodicY(-self.rval[s,ps,1]+self.rval[u+s,pu,1]))+1.0j*qval[2]*(self.geom.ApplyPeriodicZ(-self.rval[s,ps,2]+self.rval[u+s,pu,2]))))/(self.N*smax)
+							SelfInt[u] += np.sum(np.exp(1.0j*qval[0]*(self.geom.ApplyPeriodicX(-self.rval[s,ps,0]+self.rval[u+s,pu,0]))+1.0j*qval[1]*(self.geom.ApplyPeriodicY(-self.rval[s,ps,1]+self.rval[u+s,pu,1]))+1.0j*qval[2]*(self.geom.ApplyPeriodicZ(-self.rval[s,ps,2]+self.rval[u+s,pu,2]))))
 						else:
-							SelfInt[u] += np.sum(np.sum(np.exp(1.0j*qval[0]*(-self.rval[s,ps,0]+self.rval[u+2,pu,0])+1.0j*qval[1]*(-self.rval[s,ps,1]+self.rval[u+s,pu,1])+1.0j*qval[2]*(-self.rval[s,ps,2]+self.rval[u+s,pu,2]))))/(self.Ntrack*smax)
+							SelfInt[u] += np.sum(np.exp(1.0j*qval[0]*(-self.rval[s,ps,0]+self.rval[u+2,pu,0])+1.0j*qval[1]*(-self.rval[s,ps,1]+self.rval[u+s,pu,1])+1.0j*qval[2]*(-self.rval[s,ps,2]+self.rval[u+s,pu,2])))
+					SelfInt[u]=SelfInt[u]/(self.Ntrack*smax)
 				else:
 					if self.geom.periodic:
-						SelfInt[u]=np.sum(np.sum(np.exp(1.0j*qval[0]*(self.geom.ApplyPeriodicX(-self.rval[:smax,self.usethese,0]+self.rval[u:,self.usethese,0]))+1.0j*qval[1]*(self.geom.ApplyPeriodicY(-self.rval[:smax,self.usethese,1]+self.rval[u:,self.usethese,1]))+1.0j*qval[2]*(self.geom.ApplyPeriodicZ(-self.rval[:smax,self.usethese,2]+self.rval[u:,self.usethese,2]))),axis=1),axis=0)/(self.N*smax)
+						SelfInt[u]=np.sum(np.sum(np.exp(1.0j*qval[0]*(self.geom.ApplyPeriodicX(-self.rval[:smax,self.usethese,0]+self.rval[u:,self.usethese,0]))+1.0j*qval[1]*(self.geom.ApplyPeriodicY(-self.rval[:smax,self.usethese,1]+self.rval[u:,self.usethese,1]))+1.0j*qval[2]*(self.geom.ApplyPeriodicZ(-self.rval[:smax,self.usethese,2]+self.rval[u:,self.usethese,2]))),axis=1),axis=0)/(self.Ntrack*smax)
 					else:
-						SelfInt[u]=np.sum(np.sum(np.exp(1.0j*qval[0]*(-self.rval[:smax,self.usethese,0]+self.rval[u:,self.usethese,0])+1.0j*qval[1]*(-self.rval[:smax,self.usethese,1]+self.rval[u:,self.usethese,1])+1.0j*qval[2]*(-self.rval[:smax,self.usethese,2]+self.rval[u:,self.usethese,2])),axis=1),axis=0)/(Ntrack*smax)
+						SelfInt[u]=np.sum(np.sum(np.exp(1.0j*qval[0]*(-self.rval[:smax,self.usethese,0]+self.rval[u:,self.usethese,0])+1.0j*qval[1]*(-self.rval[:smax,self.usethese,1]+self.rval[u:,self.usethese,1])+1.0j*qval[2]*(-self.rval[:smax,self.usethese,2]+self.rval[u:,self.usethese,2])),axis=1),axis=0)/(self.Ntrack*smax)
                         
 		
 		#print tval